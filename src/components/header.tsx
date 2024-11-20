ad1a56b3cf10ac0edd0c9ab8e91b7067e6f73a42
picascaz <lzymelo@proton.me>
2024-11-20 10:16:38 +0800

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import * as Popover from "@radix-ui/react-popover";
 const sections = [
 
     { label: 'Home', href: '/' },
-    { label: 'About Us', href: '/About' },
+    { label: 'About Us', href: '/aboutUs' },
     { label: 'Docs', href: 'https://docs.enreach.network' },
     { label: 'Sign In', href: '' },
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import { Layout } from './Layout.tsx'
-import { About } from './pages/About.tsx'
+import { About } from './pages/AboutUs.tsx'
 
 const router = createBrowserRouter([
   {
     element: <Layout />, children: [
       { path: '/', element: <App /> },
-      { path: '/about', element: <About /> },
+      { path: '/aboutUs', element: <About /> },
     ]
   }
 ])
