4eea5c977541510ba395dc8c0ab9df18d814fd71
picascaz <lzymelo@proton.me>
2024-11-20 17:45:01 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "./docs",
+    rollupOptions: {
+      input: {
+        main: "src/main.tsx",
+        aboutUs: "src/aboutUs.tsx",
+      },
+    },
   },
   base: "./",
 });
