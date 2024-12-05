840a6064d105a87caa69e9728d326d331b82ed57
picascaz <lzymelo@proton.me>
2024-12-05 19:46:03 +0800

    fix

--- src/app/components/aboutUs/ABanner.tsx
+++ src/app/components/aboutUs/ABanner.tsx
@@ -1,10 +1,10 @@
 const ABanner = () => {
   return <div data-aos="fade-up"
     data-aos-duration="1000"
-    className=" aos-init aos-animate w-container m-auto md:w-full">
-    <div className="flex justify-between w-full items-center ">
+    className="m-auto  aos-init aos-animate w-container md:w-full">
+    <div className="flex items-center justify-between w-full ">
       <div className="bg-[#1B1B1B] p-20 md:p-0  w-[50%] md:h-[700px]">
-        <img src="ai.gif" className="" />
+        <img src="./ai.gif" className="" />
       </div>
       <div className="w-[50%]">
         <div className="h-[700px] md:mx-[30px] flex  flex-col justify-center md:pl-[10px] items-baseline  gap-[40px] md:gap-5">
@@ -23,7 +23,7 @@ const ABanner = () => {
           </span>
           <div className=" ml-[54px] md:ml-0 mt-5">
             <div className="text-[#7C7C7C] font-normal asText text-[22px] leading-normal">
-              <div className=" mb-5">
+              <div className="mb-5 ">
                 EnReach bridges the gap between centralized cloud systems and the needs of next-generation applications. As AI reshapes industries, we bring the cloud closer to users, enabling instant responses, seamless interactions, and scalable performance.
               </div>
 
