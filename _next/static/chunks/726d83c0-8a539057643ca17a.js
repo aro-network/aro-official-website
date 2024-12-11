51dc2d5c85fa241f1d308d2820e3284b558882ea
picascaz <picascaz@users.noreply.github.com>
2024-12-11 06:57:57 +0000

    deploy: 851cf3ae3177a0b83b8dba1155a0aa666afdb604

---
+++ _next/static/chunks/726d83c0-8a539057643ca17a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[80],{7518:(t,e,r)=>{r.d(e,{fcC:()=>o});var n=r(2320);function o(t){return(0,n.k5)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M4 6h16M4 12h16M4 18h16"}}]})(t)}}}]);
