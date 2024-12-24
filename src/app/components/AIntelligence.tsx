f227a301fe1c651084925b51ced08d7b0ca4fd50
picascaz <lzymelo@proton.me>
2024-12-24 15:11:24 +0800

    update texts

--- src/app/components/AIntelligence.tsx
+++ src/app/components/AIntelligence.tsx
@@ -17,35 +17,25 @@ const AIntelligence = () => {
 
       </div>
       <div className="w-[60%]  mo:w-full mo:px-5  h-[600px] mo:h-auto md:mx-[30px] flex  flex-col lg:justify-center md:pl-[10px] lg:items-baseline mo:mt-10">
-        <span className=" mo:hidden font-semibold  text-[70px] mo:text-[22px] mo:leading-normal  md:text-[50px] md:leading-[70px] leading-[91.84px]  text-[#FFFFFF]">
-          Fueling  <span className="text-[#4281FF] mr-4">
-            AI Apps
-          </span>
-          <div >
-
-            <span>
-              at the Edge
-            </span>
-
-          </div>
-
-        </span>
-        <span className="lg:hidden text-center w-full font-semibold mo:text-[22px] mo:leading-normal  text-[#FFFFFF]">
+        <span className=" font-semibold mo:text-center  text-[70px] mo:text-[22px] mo:leading-normal  md:text-[50px] md:leading-[70px] leading-[91.84px]  text-[#FFFFFF]">
           Fueling  <span className="text-[#4281FF] mr-2">
-            AI Apps
+            Edge Apps
           </span>
-          <span>
-            at the Edge
+          &
+          <span className="text-[#4281FF] ml-2 ">
+            AI
           </span>
 
+
         </span>
+
         <div className=" ml-[54px] mo:mx-5 mr-[142px] md:ml-0 mt-5 mb-10">
           <div className="text-[#7C7C7C] font-normal mo:text-center mo:text-[10px] mo:leading-[150%] font-AlbertSans text-2xl leading-7">
             A permissionless network enables diverse AI Apps to thrive. EnReach redefines digital experiences by delivering AI Apps from the edge.
             <div className="flex gap-5 mt-5 w-full mo:justify-center ">
 
               {list.map((item, index) => {
-                return <div key={`text_${index}`} className="border-[#4281FF] cursor-default border  text-[#FFFFFF]  w-[190px] mo:w-[100px] items-center leading-9  flex justify-center h-[50px] mo:h-[28px]  rounded-3xl  font-AlbertSans p-3">
+                return <div key={`text_${index}`} className="border-[#4281FF] cursor-default border  text-[#FFFFFF]  w-[190px] mo:w-[120px] items-center leading-9  flex justify-center h-[50px] mo:h-[28px]  rounded-3xl  font-AlbertSans p-3 mo:p-0">
                   {item.title}
                 </div>
 
