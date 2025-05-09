7a69e9ad3e11e874f9255b861abe121e56c94aef
picascaz <158241034+picascaz@users.noreply.github.com>
2025-05-09 23:34:36 +0800

    Update AEarly.tsx

--- src/app/components/AEarly.tsx
+++ src/app/components/AEarly.tsx
@@ -1,62 +1,69 @@
 const AEarly = () => {
-
-
-  return <div
-    data-aos="fade-up"
-    data-aos-duration="1000"
-    className="  aos-init aos-animate   w-container m-auto flex px-[170px] mo:px-5 pt-[6.25rem]  md:px-[40px] md:w-full  mo:w-full  flex-col">
-    <div className="w-full  absolute ng:hidden flex justify-end  mt-10 translate-x-[-40px] ">
-      <img src="../spots.svg" width={118} height={118} className=" object-contain relative top-[-6.25rem] w-[7.375rem] h-[7.375rem] " />
-    </div>
-    <div className=" bg-[#1C73FF0F] rounded-[2.5rem] py-6 px-11 mo:px-5">
-
-      <div className="flex">
-        <div className=" font-Ubuntu text-[2.75rem] font-medium mo:w-full text-[#1C73FF] ">
-          You’re Early
-        </div>
-
+  return (
+    <div
+      data-aos="fade-up"
+      data-aos-duration="1000"
+      className="  aos-init aos-animate   w-container m-auto flex px-[170px] mo:px-5 pt-[6.25rem]  md:px-[40px] md:w-full  mo:w-full  flex-col"
+    >
+      <div className="w-full  absolute ng:hidden flex justify-end  mt-10 translate-x-[-40px] ">
+        <img
+          src="../spots.svg"
+          width={118}
+          height={118}
+          className=" object-contain relative top-[-6.25rem] w-[7.375rem] h-[7.375rem] "
+        />
       </div>
-      <div className="flex gap-[4.875rem]  mo:h-full w-full justify-between">
-        <div>
-          <div className="text-black text-base font-bold leading-6 pt-[.625rem]">
-            Only 100 spots available for our first Pioneers cohort.
+      <div className=" bg-[#1C73FF0F] rounded-[2.5rem] py-6 px-11 mo:px-5">
+        <div className="flex">
+          <div className=" font-Ubuntu text-[2.75rem] font-medium mo:w-full text-[#1C73FF] ">
+            You’re Early
           </div>
-          <div className="text-[#000000] text-base pt-5">
-            <div>
-              As a Pioneer, you’ll get:
+        </div>
+        <div className="flex gap-[4.875rem]  mo:h-full w-full justify-between">
+          <div>
+            <div className="text-black text-base font-bold leading-6 pt-[.625rem]">
+              Only 100 spots available for our first{" "}
+              <button
+                onClick={() =>
+                  window.open("https://docs.enreach.network/campaigns/pioneer")
+                }
+                className=" underline underline-offset-1"
+              >
+                Pioneers cohort
+              </button>
+              .
             </div>
-            <div>
-              •  Early access to Edge Nodes hardware and exclusive rewards.
+            <div className="text-[#000000] text-base pt-5">
+              <div>As a Pioneer, you’ll get:</div>
+              <div>
+                • Early access to Edge Nodes hardware and exclusive rewards.
+              </div>
+              <div>
+                • Become a co-creator of EnReach and get rewarded for your
+                contributions.
+              </div>
             </div>
-            <div>
-              •  Become a co-creator of EnReach and get rewarded for your contributions.
+            <div className="text-[#1C73FF] pt-5 text-base">
+              Be more than a user.
             </div>
-          </div>
-          <div className="text-[#1C73FF] pt-5 text-base">
-            Be more than a user.
-          </div>
-          <div className="text-[#1C73FF] text-base">
+            <div className="text-[#1C73FF] text-base">
+              Help build the new open internet.
+            </div>
 
-            Help build the new open internet.
-          </div>
-
-          {/* <div className="text-[#1C73FF] text-base underline underline-offset-1 mt-5">
+            {/* <div className="text-[#1C73FF] text-base underline underline-offset-1 mt-5">
             Learn more about Pioneer Program
           </div> */}
+          </div>
         </div>
-
-
-
       </div>
-
-
+      <div className=" flex mo:hidden relative w-full  bg-fuchsia-300  ">
+        <img
+          src="../spots.svg"
+          className=" absolute  w-[16.375rem] h-[16.375rem] right-0 top-[-230px] mr-10 "
+        />
+      </div>
     </div>
-    <div className=" flex mo:hidden relative w-full  bg-fuchsia-300  ">
-      <img src="../spots.svg" className=" absolute  w-[16.375rem] h-[16.375rem] right-0 top-[-230px] mr-10 " />
-    </div>
+  );
+};
 
-
-  </div>
-}
-
-export default AEarly
+export default AEarly;
