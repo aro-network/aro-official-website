30b78f61494d848fa36d635b176af676962fcfd3
Colton <lzymelo@proton.me>
2025-05-16 13:45:58 +0800

    feat:add web app

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 
 import { useEffect } from "react";
-
+interface NavigatorWithStandalone extends Navigator {
+  standalone?: boolean;
+}
 const App = () => {
   useEffect(() => {
     const isPWA =
       window.matchMedia("(display-mode: standalone)").matches ||
-      window.navigator.standalone === true;
-
+      (window.navigator as NavigatorWithStandalone).standalone === true;
     if (!isPWA) {
       window.location.href = "/";
     }
   }, []);
 
-
-  return <iframe
-    src="https://devnet.dashboard.enreach.network/"
-    style={{ width: "100vw", height: "100vh", border: "none" }}
-    allow="fullscreen"
-  />
+  return (
+    <iframe
+      src="https://devnet.dashboard.enreach.network/"
+      style={{ width: "100vw", height: "100vh", border: "none" }}
+      allow="fullscreen"
+    />
+  );
 };
 
 export default App;
