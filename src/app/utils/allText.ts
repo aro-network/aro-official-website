db9af2ad13e038f1d310490751e323519059c70b
Colton <lzymelo@proton.me>
2025-06-04 11:11:39 +0800

    all text

---
+++ src/app/utils/allText.ts
@@ -0,0 +1,135 @@
+// export const AllText = {
+//   banner: {
+//     title: "The New Edge of AI",
+//     subTitle: "Share Your Idle Internet, Earn Rewards",
+//     btn: "Join the Pioneers Program",
+//   },
+//   about: {
+//     tab1: {
+//       title: "What Is ARO?",
+//       contnet: [
+//         "ARO is building a decentralized, intelligent edge cloud for real-time content and AI delivery. Backed by a team with $140M+ in annual revenue and 1.5M+ edge nodes deployed.",
+//         "We’re not pitching a future — we’re scaling a proven foundation.Now open to the crowd.",
+//       ],
+//     },
+//     tab2: {
+//       title: "Why It Matters",
+//       content: [
+//         "• $154.7B market for Edge Cloud by 2030",
+//         "• $66.4B market for Edge AI by 2030",
+//         "(Grand View Research, Gartner, Sky Quest)",
+//         "Backed by a proven team:",
+//         "• 1.5M+ nodes deployed",
+//         "• $140M+ in annual revenue",
+//         "• Real enterprise demand secured",
+//       ],
+//     },
+//   },
+//   runSteps: {
+//     step1: {
+//       title: "Plug it.",
+//       subTitle: "Hardware or Software Client, your choice.",
+//       content: [
+//         {
+//           title: "Berry Box",
+//           content:
+//             "Dedicated plug-and-play device. Optimized for high uptime and rewards.",
+//         },
+//         {
+//           title: "Software Node",
+//           content:
+//             "Install on your own device. Simple, flexible, and easy to start.",
+//         },
+//         {
+//           title: "Berry Router",
+//           content: "WiFi router with built-in ARO node. Earn while you browse.",
+//         },
+//       ],
+//       tips: "Hardware images shown are for reference only. Final design and specs may vary.",
+//     },
+//     step2: {
+//       title: "Install it.",
+//       tips: "Contribute your idle bandwidth as an Enreach Edge Node.",
+//     },
+//     step3: {
+//       title: "Start Earning.",
+//       content: [
+//         {
+//           title: "Earn $ARO Rewards",
+//           content:
+//             "Stack Berry points during Testnet, convert them to $ARO tokens at mainnet launch.",
+//         },
+//         {
+//           content: "Refer your friend and earn extra 15% commission.",
+//         },
+//       ],
+//     },
+//   },
+
+//   joinPioneers: {
+//     title: "You’re Early",
+//     subtitle: "Only 100 spots available for our first Pioneers cohort .",
+//     content: {
+//       title: "As a Pioneer, you’ll get:",
+//       content: [
+//         " • Early access to Edge Nodes hardware and exclusive rewards.",
+//         " • Become a co-creator of ARO and get rewarded for your contributions.",
+//       ],
+//       tips: ["Be more than a user.", "Help build the new open internet."],
+//     },
+//   },
+
+//   becomePioneerSteps: {
+//     step1: "Step 1: Join the ARO Community",
+//     step2: "Step 2: Submit your details and fill out the application form",
+//     text: "Yes, I want to join the Pioneers Program. Run a node, give feedback, and be early 👀",
+//   },
+//   roadmap: [
+//     {
+//       date: "2024",
+//       title: "Laying the Foundations",
+//       items: [
+//         "GPoW (Geographic Proof of Work) & GPoS (Geographic Proof of Stake)",
+//         "Peer-HVM (Hardware Virtual Machine)",
+//         "Peer-DTS (Distributed Task Scheduling)",
+//         "Peer-Routing (Decentralized Routing Layer)",
+//       ],
+//       color: "bg-[#8D9CB6]",
+//       finished: true,
+//     },
+//     {
+//       date: "2025 Q1",
+//       title: "Activating the Network",
+//       items: [
+//         "Community launch",
+//         "Initial hardware offering",
+//         "Resource aggregation",
+//       ],
+//       color: "bg-[#8D9CB6]",
+//       finished: true,
+//     },
+//     {
+//       date: "2025 Q2",
+//       title: "Proving Our Model",
+//       items: [
+//         "Real demand: PCDN delivery and AI hosting",
+//         "Kickstarting onchain revenue",
+//         "Testnet launch",
+//       ],
+//       color: "bg-[#1C73FF]",
+//       isCurrent: true,
+//     },
+//     {
+//       date: "2025 Q3",
+//       title: "Scaling Globally",
+//       items: ["Mainnet launch", "Node expansion", "Customer expansion"],
+//       color: "bg-[#5995F5]",
+//     },
+//     {
+//       date: "2026",
+//       title: "Opening the Network",
+//       items: ["Open platform launch", "Open governance"],
+//       color: "bg-[#5995F5]",
+//     },
+//   ],
+// };
