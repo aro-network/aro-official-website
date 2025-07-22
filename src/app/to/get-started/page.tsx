33d803d79eab79cfadef90ccd7868f685c337d23
Colton <lzymelo@proton.me>
2025-07-22 17:32:24 +0800

    [all] add new link

--- src/app/to/get-started/page.tsx
+++ src/app/to/get-started/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react"
 const AGetStarted = () => {
 
   useEffect(() => {
-    window.open('https://devnet-dashboard.aro.network/signup', '_self')
+    window.open('https://dashboard.aro.network/signup', '_self')
   })
   return null
 
