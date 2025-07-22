6018d43e0b0873bdfc6357c128bccdab063d4d14
Colton <lzymelo@proton.me>
2025-07-22 17:34:33 +0800

    [all] add new link

---
+++ src/app/to/event/gmvn2025/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useEffect } from "react"
+
+const AGetStarted = () => {
+
+  useEffect(() => {
+    window.open('https://order.aro.network/aro-pod/?ref=gmvn25', '_self')
+  })
+  return null
+}
+
+export default AGetStarted
