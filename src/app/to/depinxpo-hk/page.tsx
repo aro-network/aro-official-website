f374b19da780d799bde170b9e7faf9932709cee9
Colton <lzymelo@proton.me>
2025-08-14 13:59:44 +0800

    [all] add new link

---
+++ src/app/to/depinxpo-hk/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useEffect } from "react"
+
+const AGetStarted = () => {
+
+  useEffect(() => {
+    window.open('https://dashboard.aro.network/signup/?utm_source=depinexpo&utm_medium=offline&utm_campaign=hk2025', '_self')
+  })
+  return null
+
+}
+
+export default AGetStarted
