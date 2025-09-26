35c2c724ead37baee53bacfe36e9a5416865b1d9
Colton <lzymelo@proton.me>
2025-09-26 22:55:56 +0800

    [beta] update link

--- src/app/utils/allLink.ts
+++ src/app/utils/allLink.ts
@@ -4,7 +4,7 @@ const use = "https://docs-stage.aro.network";
 export const AllLink = {
   docs: use,
   podUserGuide: `${use}/node-operator-guide/aro-pod/get-aro-pod`,
-  clientUserGuide: `${use}/node-operator-guide/aro-client/choose-installation-method`,
+  clientUserGuide: `${use}/node-operator-guide/aro-client/aro-client-introduction`,
   linkUserGuide: `${use}/node-operator-guide/aro-link`,
   liteUserGuide: `${use}/node-operator-guide/aro-lite/get-aro-lite`,
   podSeeGuidance: `${use}/node-operator-guide/aro-pod/aro-pod-installation-guide`,
